40ed38cfc875b2fc51e7318e1e71bd7bf436fc04
rikapro0128 <stkssc002@gmail.com>
2022-03-08 10:06:55 +0700

    save code

---
+++ USB_Control/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace USB_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Status = new System.Windows.Forms.TextBox();
+            this.textBox_VID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_PID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_SW = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.toolStripStatusLabel_InforDevice = new System.Windows.Forms.StatusStrip();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.usbHidPort1 = new UsbLibrary.UsbHidPort(this.components);
+            this.usbHidPort2 = new UsbLibrary.UsbHidPort(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox_PID);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox_VID);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox_Status);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 163);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Device Control";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_SW);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Location = new System.Drawing.Point(268, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(163, 54);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Switch Status";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Device Status";
+            // 
+            // textBox_Status
+            // 
+            this.textBox_Status.BackColor = System.Drawing.Color.Red;
+            this.textBox_Status.ForeColor = System.Drawing.Color.Black;
+            this.textBox_Status.Location = new System.Drawing.Point(86, 24);
+            this.textBox_Status.Name = "textBox_Status";
+            this.textBox_Status.Size = new System.Drawing.Size(155, 20);
+            this.textBox_Status.TabIndex = 1;
+            this.textBox_Status.Text = "Disconnected!";
+            this.textBox_Status.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox_VID
+            // 
+            this.textBox_VID.Location = new System.Drawing.Point(86, 50);
+            this.textBox_VID.Name = "textBox_VID";
+            this.textBox_VID.Size = new System.Drawing.Size(65, 20);
+            this.textBox_VID.TabIndex = 3;
+            this.textBox_VID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Vendor ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(157, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "For MikroC";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(157, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "For PIC18F4550";
+            // 
+            // textBox_PID
+            // 
+            this.textBox_PID.Location = new System.Drawing.Point(86, 76);
+            this.textBox_PID.Name = "textBox_PID";
+            this.textBox_PID.Size = new System.Drawing.Size(65, 20);
+            this.textBox_PID.TabIndex = 6;
+            this.textBox_PID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Product ID";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(86, 128);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(155, 20);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Product Name";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(86, 102);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(155, 20);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 105);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Vendor Name";
+            // 
+            // textBox_SW
+            // 
+            this.textBox_SW.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox_SW.Location = new System.Drawing.Point(91, 24);
+            this.textBox_SW.Name = "textBox_SW";
+            this.textBox_SW.Size = new System.Drawing.Size(65, 20);
+            this.textBox_SW.TabIndex = 5;
+            this.textBox_SW.Text = "0";
+            this.textBox_SW.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(11, 27);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Counter SW";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBox2);
+            this.groupBox3.Controls.Add(this.pictureBox1);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Location = new System.Drawing.Point(268, 72);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(163, 103);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Led Control";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 23);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "LED On/Off";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ON";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ON_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(81, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "OFF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OFF_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(356, 181);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Exit Program";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // toolStripStatusLabel_InforDevice
+            // 
+            this.toolStripStatusLabel_InforDevice.Location = new System.Drawing.Point(0, 207);
+            this.toolStripStatusLabel_InforDevice.Name = "toolStripStatusLabel_InforDevice";
+            this.toolStripStatusLabel_InforDevice.Size = new System.Drawing.Size(437, 22);
+            this.toolStripStatusLabel_InforDevice.TabIndex = 4;
+            this.toolStripStatusLabel_InforDevice.Text = "statusStrip1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(6, 47);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(70, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // usbHidPort1
+            // 
+            this.usbHidPort1.ProductId = 0;
+            this.usbHidPort1.VendorId = 0;
+            // 
+            // usbHidPort2
+            // 
+            this.usbHidPort2.ProductId = 0;
+            this.usbHidPort2.VendorId = 0;
+            this.usbHidPort2.OnSpecifiedDeviceArrived += new System.EventHandler(this.usbHidPort1_OnSpecifiedDeviceArrived);
+            this.usbHidPort2.OnSpecifiedDeviceRemoved += new System.EventHandler(this.usbHidPort1_OnSpecifiedDeviceRemoved);
+            this.usbHidPort2.OnDeviceArrived += new System.EventHandler(this.usbHidPort1_OnDeviceArrived);
+            this.usbHidPort2.OnDeviceRemoved += new System.EventHandler(this.usbHidPort1_OnDeviceRemoved);
+            this.usbHidPort2.OnDataRecieved += new UsbLibrary.DataRecievedEventHandler(this.usbHidPort1_OnDataRecieved);
+            this.usbHidPort2.OnDataSend += new System.EventHandler(this.usbHidPort1_OnDataSend);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 229);
+            this.Controls.Add(this.toolStripStatusLabel_InforDevice);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Tag = "0";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_PID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_VID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Status;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_SW;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.StatusStrip toolStripStatusLabel_InforDevice;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private UsbLibrary.UsbHidPort usbHidPort1;
+        private UsbLibrary.UsbHidPort usbHidPort2;
+    }
+}
+
---
+++ USB_Control/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UsbLibrary;
+
+namespace USB_Control
+{
+    public partial class Form1 : Form
+    {
+        byte[] readbuff = new byte[65];
+        byte[] writebuff = new byte[65];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.usbHidPort2.VendorId = 0x04d8;
+            this.usbHidPort2.ProductId = 0x0001;
+            this.usbHidPort2.CheckDevicePresent();
+
+            if (this.usbHidPort2.SpecifiedDevice != null)
+            {
+                this.usbHidPort2.SpecifiedDevice.SendData(writebuff);
+            }
+            textBox_VID.Text = usbHidPort1.VendorId.ToString("x4");
+            textBox_PID.Text = usbHidPort1.ProductId.ToString("x4");
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            DialogResult answer = MessageBox.Show("Do you want to exit the program?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (answer == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void ON_Click(object sender, EventArgs e)
+        {
+            writebuff[1] = 1;
+            if (this.usbHidPort2.SpecifiedDevice != null)
+                this.usbHidPort2.SpecifiedDevice.SendData(writebuff);
+            else
+            {
+                MessageBox.Show("Device not found. Please reconnect USB device to use.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void OFF_Click(object sender, EventArgs e)
+        {
+            writebuff[1] = 0;
+            if (this.usbHidPort2.SpecifiedDevice != null)
+                this.usbHidPort2.SpecifiedDevice.SendData(writebuff);
+            else
+            {
+                MessageBox.Show("Device not found. Please reconnect USB device to use.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void usbHidPort1_OnDataRecieved(object sender, DataRecievedEventArgs args)
+        {
+            if (InvokeRequired)
+            {
+                try
+                {
+                    Invoke(new DataRecievedEventHandler(usbHidPort1_OnDataRecieved), new object[] { sender, args });
+                }
+                catch { }
+
+            }
+            else
+            {
+                readbuff = args.data;
+                toolStripStatusLabel_InforDevice.Text = "New Receieved Data";
+                textBox_SW.Text = readbuff[1].ToString();
+                if (readbuff[9] == 'O')
+                {
+                    //ovalShape1.BackColor = Color.Red;
+                    pictureBox2.Visible = false;
+                }
+                else if (readbuff[9] == 'F')
+                {
+                    //ovalShape1.BackColor = Color.DarkGray;
+                    pictureBox2.Visible = true;
+                }
+            }
+        }
+
+        private void usbHidPort1_OnDataSend(object sender, EventArgs e)
+        {
+            toolStripStatusLabel_InforDevice.Text = "Data sent";
+        }
+
+        private void usbHidPort1_OnDeviceArrived(object sender, EventArgs e)
+        {
+            toolStripStatusLabel_InforDevice.Text = "USB Connected";
+        }
+
+        private void usbHidPort1_OnDeviceRemoved(object sender, EventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new EventHandler(usbHidPort1_OnDeviceRemoved), new object[] { sender, e });
+
+            }
+            else
+            {
+                toolStripStatusLabel_InforDevice.Text = "USB Removed";
+            }
+        }
+
+        private void usbHidPort1_OnSpecifiedDeviceArrived(object sender, EventArgs e)
+        {
+            toolStripStatusLabel_InforDevice.Text = "Device Detected";
+            textBox_Status.Text = "Connected!";
+            textBox_Status.BackColor = Color.Lime;
+            textBox_SW.Text = "0";
+            //ovalShape1.BackColor = Color.DarkGray;
+            pictureBox2.Visible = true;
+        }
+
+        private void usbHidPort1_OnSpecifiedDeviceRemoved(object sender, EventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new EventHandler(usbHidPort1_OnSpecifiedDeviceRemoved), new object[] { sender, e });
+
+            }
+            else
+            {
+                toolStripStatusLabel_InforDevice.Text = "Device Disconnected";
+                textBox_Status.Text = "Disconnected!!";
+                textBox_Status.BackColor = Color.Red;
+            }
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            usbHidPort2.RegisterHandle(Handle);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            usbHidPort2.ParseMessages(ref m);
+            base.WndProc(ref m);
+        }
+    }
+}
